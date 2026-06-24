ca443a220a1860ee95d79c2f9cd7e03092d95357
anokabb <anokabb@users.noreply.github.com>
2026-06-24 00:05:36 +0000

    deploy: 7bfcba39a29e075c04630bc6f07e0d2cc36745d2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3011360796"
+    serviceWorkerVersion: "3458877190"
   },
   config: {
     // Serve CanvasKit from this app's own bundle instead of fetching it from
